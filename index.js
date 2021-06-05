8d08f206cf2b95080be68dce7ff9c102e03724c4
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-06-04 20:49:05 -0400

    1 file modified

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const redo = new Stack();
 const editor = document.getElementById("editor");
 
 editor.addEventListener("keydown", ev => {
-  if (ev.ctrlKey && ev.key === "z") {
+  if ((ev.ctrlKey || ev.metaKey) && ev.key === "z") {
     ev.preventDefault();
     // Here is where we're going to link the undo functionality
     // with the front-end.  In this event listener, we need to
@@ -56,7 +56,7 @@ editor.addEventListener("keydown", ev => {
     }
     // Don't forget!  We also have to make sure we're loading
     // our redo stack as well!
-  } else if (ev.ctrlKey && ev.key === "r") {
+  } else if ((ev.ctrlKey || ev.metaKey) && ev.key === "r") {
     ev.preventDefault();
     // Just like you did with the undo functionality, link your redo
     // stack functionality here!  Remember!  We want to set
@@ -71,7 +71,7 @@ editor.addEventListener("keydown", ev => {
     // our UNDO stack as well!
   } else {
     // Else we should just send ev.target.value to our undo object!
-    undo.push(ev.target.value).print()
+    undo.push(editor.value).print()
     
   }
 });
